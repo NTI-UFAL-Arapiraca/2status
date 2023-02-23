44641df297ef8e6cd5ac111e5a6be9e5e2e1ab2f
Charlie Bellow <charliebftm@gmail.com>
2023-02-23 13:36:16 -0300

    adding function to modal

---
+++ index.js
@@ -0,0 +1,35 @@
+const Modal = {
+
+  togleModal () {
+    const modalClass = document
+      .querySelector('.modal-overlay');
+      
+    if (modalClass.classList.incldes("active")) {
+      modalClass.classList.remove("active");
+    } else {
+      document
+        .querySelector('.modal-overlay')
+        .classList
+        .add('active');
+    }
+  }
+
+  open () {
+    // abrir modal
+    // adicionar a classe active ao modal
+    document
+      .querySelector('.modal-overlay')
+      .classList
+      .add('active');
+    
+  },
+  
+  close () {
+    // fechar Modal
+    // remover a classe active no modal
+    document
+      .querySelector('.modal-overlay')
+      .classList
+      .remove('active');
+  }
+};
