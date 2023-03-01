a832ab8c586928bdd8f7ec06f7b3d8f72828c9dc
arthwrvl <joaobob63@gmail.com>
2023-03-01 09:18:01 -0300

    styling modal

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 let modal = document.getElementById("modal");
+let online = document.getElementById("online");
+let offline = document.getElementById("offline");
+let modalTitle = document.getElementById("title");
 
-function openModal(){
+function openModal(status, title){
     modal.style.display = "block";
+    modalTitle.innerHTML = title;
+    if(status){
+        online.style.display = "flex";
+        offline.style.display = "none";
+    }else{
+        online.style.display = "none";
+        offline.style.display = "flex";
+    }
 }
 function closeModal(){
     modal.style.display = "none";
 }
+document.addEventListener('keydown', (event) => {
+    if(event.key === "Escape"){
+        closeModal();
+    }
+})
